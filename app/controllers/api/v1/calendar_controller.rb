140f0a783a1057a1e5a412e5ef45f6b70f3a49f7
Harvin Llanes <harvin.llanes@gmail.com>
2017-07-03 16:59:08 -0600

    Validate properly that day is on weekend

--- app/controllers/api/v1/calendar_controller.rb
+++ app/controllers/api/v1/calendar_controller.rb
@@ -1,6 +1,10 @@
 class API::V1::CalendarController < ApplicationController
   def index
-    CalendarCreatorJob.perform(Time.current.to_s)
-    render json: {success: true}
+    render json: CalendarItem.all
+  end
+
+  def create
+    CalendarCreatorJob.perform_later(params[:start_date])
+    render json: :success
   end
 end
--- app/jobs/calendar_creator_job.rb
+++ app/jobs/calendar_creator_job.rb
@@ -7,7 +7,7 @@ class CalendarCreatorJob < ApplicationJob
     start_date = Date.parse(start_date_string)
     next_date = start_date
     while next_date.year <= start_date.year do
-      weekend = (next_date.wday == 6 || next_date == 7)
+      weekend = (next_date.wday == 0 || next_date.wday == 6)
       CalendarItem.create event_date: next_date, is_weekend: weekend
       next_date = next_date + 4.day
     end
