6e6b99670533c023ae7fc3807f9acfce96da2541
Harvin Llanes <harvin.llanes@gmail.com>
2017-07-05 21:38:58 -0600

    Update capistrano setup for run workers

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,4 @@
 # config valid only for current version of Capistrano
-before "deploy:restart", "bundle:install"
-namespace :bundle do
- desc "run bundle install and ensure all gem requirements are met"
- task :install do
-   run "cd #{current_path} && bundle install  --without=test --no-update-sources"
- end
-end
 lock "3.8.2"
 role :resque_worker, "52.89.198.196"
 role :resque_scheduler, "52.89.198.196"
@@ -20,5 +13,6 @@ set :deploy_via, :copy
 set :user, 'centos'
 set :ssh_options, { forward_agent: true, port: 22 }
 set :passenger_restart_with_touch, true
-set :workers, { "my_queue_name" => 2 }
+set :workers, { "default" => 2 }
+set :resque_environment_task, true
 
